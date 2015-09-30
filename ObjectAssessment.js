210db471e12173a9c5a956fef275d216b1e2985d
Dane Smith <doctorhowser@gmail.com>
2015-09-30 18:17:22 -0500

    initial commit

---
+++ ObjectAssessment.js
@@ -0,0 +1,14 @@
+var avengersMovie = { //missing var
+	name: "Avengers", //missing quotes for string, comma instead of semi-colon
+	runtime: "143 mins",//missing quotes for string, comma instead of semi-colon
+	releaseYear: 2012,//colon insead of equals sign, comma instead of semi-colon
+	director: "Joss Whedon"//property name shouldn't be a string, lowercase director, no period at end
+};//add semicolon to end assignment
+
+/* old code
+avengersMovie = {
+    name : Avengers;
+    runtime : 143 mins;
+    releaseYear = 2012;
+    "Director" : "Joss Whedon".
+}*/
