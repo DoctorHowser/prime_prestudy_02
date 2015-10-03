1ca6b456e57ac6b414c0802b0a7733433acbf534
Dane Smith <doctorhowser@gmail.com>
2015-10-03 13:22:59 -0500

    Fix Director, add more comments

--- ObjectAssessment.js
+++ ObjectAssessment.js
@@ -1,9 +1,9 @@
 var avengersMovie = { //missing var
 	name: "Avengers", //missing quotes for string, comma instead of semi-colon
-	runtime: "143 mins",//missing quotes for string, comma instead of semi-colon
+	runtime: "143 mins",//missing quotes for string, comma instead of semi-colon--I treated this like a string, but maybe there is another way?
 	releaseYear: 2012,//colon insead of equals sign, comma instead of semi-colon
-	director: "Joss Whedon"//property name shouldn't be a string, lowercase director, no period at end
-};//add semicolon to end assignment
+	"Director": "Joss Whedon"//no period at end--Initially I thought you couldn't have strings as the property, but according to mozilla, you can, and even have numbers!
+};//add semicolon to end assignment--Semicolon placement really trips me up.
 console.log(avengersMovie);
 /* old code
 avengersMovie = {
